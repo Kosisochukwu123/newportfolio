c9f134c4ae9fb412e5aa08561382e81287492cc2
Kosisochukwu123 <kosiogbunuko@gmail.com>
2026-04-01 12:21:10 +0100

    6

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const limiter = rateLimit({
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 100,
   message: { success: false, message: "Too many auth attempts, try again later." },
 });
 
